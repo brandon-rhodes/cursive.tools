d5dd5e3331d3a9f831ab76143e2d206b1eb004b8
Brandon Rhodes <brandon@rhodesmill.org>
2009-04-14 20:02:59 -0400

    Improved the word-count algorithm so that it tallies by section.

--- cursive.py
+++ cursive.py
@@ -1,33 +1,71 @@
 """The `cursive` command-line program itself."""
 
-from docutils.nodes import Text, GenericNodeVisitor
+from docutils.nodes import GenericNodeVisitor
 from docutils import core
 from docutils.writers import Writer
 
 textual_nodes = set([ 'block_quote', 'paragraph',
                       'list_item', 'term', 'definition_list_item', ])
 
+class Section(object):
+    """Maintains a counter of how many words are in a section."""
+
+    def __init__(self):
+        self.title = ''
+        self.words = 0
+        self.subsections = []
+
+    def create_subsection(self):
+        ss = Section()
+        self.subsections.append(ss)
+        return ss
+
+    def add_text(self, text):
+        self.words += len(text.split())
+
+    def total(self):
+        return sum([ self.words ] + [ ss.words for ss in self.subsections ])
+
+    def report(self):
+        title = self.title
+        if len(title) > 58:
+            title = title[:57] + '\\'
+        wordstr = str(self.total())
+        dots = '.' * (68 - len(title) - len(wordstr) - 7)
+        return ('%s %s %s words\n' % (title, dots, wordstr) +
+                ''.join( '    ' + ss.report() for ss in self.subsections ))
+
 class MyVisitor(GenericNodeVisitor):
     def __init__(self, *args, **kw):
-        self.words = 0
+        self.sections = [ Section() ]
         GenericNodeVisitor.__init__(self, *args, **kw)
 
-    def default_visit(self, node):
-        if (isinstance(node, Text)
-            and (node.parent.tagname in textual_nodes)):
-            self.words += len(node.astext().split())
+    def visit_title(self, node):
+        self.sections[-1].title = node.astext()
 
-    def default_departure(self, node):
-        pass
+    def visit_section(self, node):
+        sections = self.sections
+        ss = sections[0].create_subsection()
+        sections.append(ss)
+
+    def depart_section(self, node):
+        self.sections.pop()
+
+    def visit_Text(self, node):
+        if node.parent.tagname in textual_nodes:
+            self.sections[-1].add_text(node.astext())
+
+    def default_visit(self, node): pass
+    def default_departure(self, node): pass
 
     def astext(self):
-        return '%d words\n' % self.words
+        return self.sections[0].report()
 
 class MyWriter(Writer):
     def translate(self):
         visitor = MyVisitor(self.document)
         self.document.walkabout(visitor)
-        self.output = visitor.astext()
+        self.output = '\n' + visitor.astext() + '\n'
 
 def console_script_cursive():
     core.publish_cmdline(writer=MyWriter())
