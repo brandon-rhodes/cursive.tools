3cfc7690bd29422af0404bda07a13dea19400036
Brandon Rhodes <brandon@rhodesmill.org>
2009-04-24 13:48:25 -0400

    Added a project URL.

--- __init__.py
+++ __init__.py
@@ -5,3 +5,4 @@ __version__ = '0.1'
 __testrunner__ = 'nose'
 __requires__ = [ 'docutils' ]
 __author__ = 'Brandon Craig Rhodes <brandon@rhodesmill.org>'
+__url__ = 'http://bitbucket.org/brandon/cursivetools/'
